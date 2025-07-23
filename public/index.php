0d73a8755106252417ba0d9cbf1763eea31fa31c
maxheong54 <maxheong54@gmail.com>
2025-07-23 20:56:30 +0300

    Set up templating and add main page

--- public/index.php
+++ public/index.php
@@ -1,15 +1,24 @@
 <?php
 
+use DI\Container;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$container = new Container();
+
+$container->set(Twig::class, fn () => Twig::create(__DIR__ . '/../templates'));
+
+$app = AppFactory::createFromContainer($container);
+
+$app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
 $app->addErrorMiddleware(true, true, true);
 
-
 $app->get('/', function ($request, $response) {
-    return $response->write('Welcome');
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'main.html.twig');
 })->setName('root');
 
 $app->run();
